cbaa05e3db1cfbf09953fe61344b7af63520df6e
amin soli <amin23877@gmail.com>
2021-04-30 12:05:38 +0430

    /g

--- src/logic/pdf.ts
+++ src/logic/pdf.ts
@@ -15,11 +15,11 @@ export const exportPdf = async (input: HTMLElement) => {
         let dY = 0;
         let dWidth = 900;
         let dHeight = 980;
-        let scaleX = canvas.width/600;
-        let scaleY = canvas.height/1034;
+        let scaleX = 600 / canvas.width;
+        let scaleY = 1034 / canvas.height;
         //we need to scale the canvas but we dont have the element :|
         const ctx1 = canvas.getContext('2d');
-        ctx1?.scale(scaleX,scaleY);
+        ctx1?.scale(scaleX, scaleY);
         canvas.width = canvas.width * scaleX;
         canvas.height = canvas.height * scaleY;
         let srcImg = canvas;
@@ -33,13 +33,13 @@ export const exportPdf = async (input: HTMLElement) => {
             // details on this usage of this function:
             // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
             ctx?.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
-            ctx?.scale(scaleX,scaleY);
-            
+            ctx?.scale(scaleX, scaleY);
+
             const canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
 
             const width = onePageCanvas.width;
             const height = onePageCanvas.height;
-            const cwidth = canvas.width ;
+            const cwidth = canvas.width;
             const cheight = canvas.height;
             //! If we're on anything other than the first page,
             // add another page
@@ -50,11 +50,11 @@ export const exportPdf = async (input: HTMLElement) => {
             pdf.setPage(i + 1);
             //! now we add content to that page!
             // pdf.addImage(canvasDataURL, "PNG", 20, 40, width * 0.62, height * 0.62);
-            pdf.addImage(canvasDataURL, "PNG", 40,30, (input.clientWidth*1.12) ,(input.clientHeight*0.62));
+            pdf.addImage(canvasDataURL, "PNG", 40, 30, (input.clientWidth * 1.12), (input.clientHeight * 0.62));
             console.log(onePageCanvas);
-            console.log(height*0.62);
+            console.log(height * 0.62);
             console.log(height);
-            console.log(width*0.62);
+            console.log(width * 0.62);
             console.log(width);
             console.log(canvas);
             console.log(input.clientHeight);
