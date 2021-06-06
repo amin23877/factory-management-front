55b066c549c4e271400359f2d680183d9f962ce9
amin soli <amin23877@gmail.com>
2021-06-06 12:07:42 +0430

    filter and fields in a big modal

--- src/features/Field/Modal.tsx
+++ src/features/Field/Modal.tsx
@@ -10,45 +10,46 @@ import { basePost } from "../../api";
 import { IField } from "../../api/field";
 import { mutate } from "swr";
 
-export const AddFieldModal = ({ open, onClose }: { open: boolean; onClose: () => void }) => {
+// { open, onClose }: { open: boolean; onClose: () => void }
+export const AddFieldModal = () => {
 
     const [activeTab, setActiveTab] = useState(0);
     const [selectedField, setSelectedField] = useState<IField>();
 
 
     return (
-        <Dialog open={open} onClose={onClose} maxWidth={activeTab === 0 ? "md" : "sm"} fullWidth title={"Add Field Modal"}>
-            <Box m={3}>
-                <Tabs value={activeTab} onChange={(e, nv) => { setActiveTab(nv); setSelectedField(undefined) }}>
-                    <Tab label="List" />
-                    <Tab label="Form" />
-                </Tabs>
-                {activeTab === 0 ?
-                    <div>
-                        <Box display="grid" gridTemplateColumns=" 1fr" gridRowGap={12} gridColumnGap={12}>
-                            <Button kind="add"
-                                onClick={() => {
-                                    setActiveTab(1)
-                                    setSelectedField(undefined)
-                                }}
-                                style={{ margin: '10px auto' }}>
-                                Add Dynamic Filed
+        // <Dialog open={open} onClose={onClose} maxWidth={activeTab === 0 ? "md" : "sm"} fullWidth title={"Add Field Modal"}>
+        <Box m={3}>
+            <Tabs value={activeTab} onChange={(e, nv) => { setActiveTab(nv); setSelectedField(undefined) }}>
+                <Tab label="List" />
+                <Tab label="Form" />
+            </Tabs>
+            {activeTab === 0 ?
+                <div>
+                    <Box display="grid" gridTemplateColumns=" 1fr" gridRowGap={12} gridColumnGap={12}>
+                        <Button kind="add"
+                            onClick={() => {
+                                setActiveTab(1)
+                                setSelectedField(undefined)
+                            }}
+                            style={{ margin: '10px auto' }}>
+                            Add Dynamic Filed
                                     </Button>
-                        </Box>
-                        <FieldTable onRowSelected={(row) => {
-                            setSelectedField(row)
-                            console.log(row)
-                            setActiveTab(1);
-                        }} />
-                    </div>
-                    :
-                    <FieldForm
-                        initial={selectedField}
-                    />
+                    </Box>
+                    <FieldTable onRowSelected={(row) => {
+                        setSelectedField(row)
+                        console.log(row)
+                        setActiveTab(1);
+                    }} />
+                </div>
+                :
+                <FieldForm
+                    initial={selectedField}
+                />
 
 
-                }
-            </Box>
-        </Dialog>
+            }
+        </Box>
+        // </Dialog>
     );
 };
