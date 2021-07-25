42d21f323a2e00d398339b2192e22892b42d0ff8
amin23877 <amin23877@gmail.com>
2021-07-25 14:49:46 +0430

    spaces

--- src/features/Engineering/Monitoring/Details.tsx
+++ src/features/Engineering/Monitoring/Details.tsx
@@ -9,68 +9,72 @@ import { BasePaper } from "../../../app/Paper";
 import { General } from "./Forms";
 
 function ItemsDetails({ selectedRow }: { selectedRow: any }) {
-    const [activeTab, setActiveTab] = useState(0);
-    const [moreInfoTab, setMoreInfoTab] = useState(0);
-    const { data: rules, mutate: mutateItems } = useSWR("/quote");
+  const [activeTab, setActiveTab] = useState(0);
+  const [moreInfoTab, setMoreInfoTab] = useState(0);
+  const { data: rules, mutate: mutateItems } = useSWR("/quote");
 
-    const gridColumns = useMemo<GridColDef[]>(() => {
-        const res: GridColDef[] = [
-            { field: "date", headerName: "Date", width: 150 },
-            { field: "name", headerName: "Name", flex: 3 },
-            { field: "number", headerName: "Number", flex: 2 },
-            { field: "unit", headerName: "Unit Serial No.", flex: 2 },
-            { field: "description", headerName: "SO", flex: 2 },
-            { field: "section", headerName: "PO", flex: 2 },
-            { field: "enable", headerName: "Pass/Fail", type: "boolean", width: 100 },
-        ];
-        return res;
-    }, [rules]);
+  const gridColumns = useMemo<GridColDef[]>(() => {
+    const res: GridColDef[] = [
+      { field: "date", headerName: "Date", width: 150 },
+      { field: "name", headerName: "Name", flex: 3 },
+      { field: "number", headerName: "Number", flex: 2 },
+      { field: "unit", headerName: "Unit Serial No.", flex: 2 },
+      { field: "description", headerName: "SO", flex: 2 },
+      { field: "section", headerName: "PO", flex: 2 },
+      { field: "enable", headerName: "Pass/Fail", type: "boolean", width: 100 },
+    ];
+    return res;
+  }, [rules]);
 
-    return (
-        <Box>
-            <Grid container spacing={2}>
-                <Grid item md={5} xs={12}>
-                    <BasePaper>
-                        <General />
-                    </BasePaper>
-                </Grid>
-                <Grid item md={7} xs={12}>
-                    <BasePaper
-                        style={{
-                            height: "100%",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <Tabs
-                            style={{ marginBottom: 16 }}
-                            value={moreInfoTab}
-                            variant="scrollable"
-                            textColor="primary"
-                            onChange={(e, v) => setMoreInfoTab(v)}
-                        >
-                            <Tab label="Equation factors modifications" />
-                        </Tabs>
-                        {moreInfoTab === 0 && <div></div>}
-                    </BasePaper>
-                </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <BasePaper>
-                        <Tabs
-                            value={activeTab}
-                            onChange={(e, v) => setActiveTab(v)}
-                            textColor="primary"
-                            variant="scrollable"
-                        >
-                            <Tab label="Rule History" />
-                        </Tabs>
-                        <Box p={3}>{activeTab === 0 && <BaseDataGrid cols={gridColumns} rows={rules || []} />}</Box>
-                    </BasePaper>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item md={5} xs={12}>
+          <BasePaper>
+            <General />
+          </BasePaper>
+        </Grid>
+        <Grid item md={7} xs={12}>
+          <BasePaper
+            style={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Tabs
+              style={{ marginBottom: 16 }}
+              value={moreInfoTab}
+              variant="scrollable"
+              textColor="primary"
+              onChange={(e, v) => setMoreInfoTab(v)}
+            >
+              <Tab label="Equation factors modifications" />
+            </Tabs>
+            {moreInfoTab === 0 && <div></div>}
+          </BasePaper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <BasePaper>
+            <Tabs
+              value={activeTab}
+              onChange={(e, v) => setActiveTab(v)}
+              textColor="primary"
+              variant="scrollable"
+            >
+              <Tab label="Rule History" />
+            </Tabs>
+            <Box p={3}>
+              {activeTab === 0 && (
+                <BaseDataGrid cols={gridColumns} rows={rules || []} />
+              )}
+            </Box>
+          </BasePaper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 export default ItemsDetails;
