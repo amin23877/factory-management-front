e21ee0e182e18c12321bb0c7ea0198c6b525c4fe
mobin <mnlarijanian@gmail.com>
2022-02-27 23:03:39 +0330

    fix on notification designator for notification button

--- src/app/TopAppBar/Notification/Button.tsx
+++ src/app/TopAppBar/Notification/Button.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Badge, IconButton } from "@material-ui/core";
 import { NotificationsOutlined } from "@material-ui/icons";
 
 import NotificationMenu from "./Menu";
+import { useSocket } from "logic/Socket/Provider";
 
 export default function NotificationButton() {
   const [showDot, setShowDot] = useState(false);
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const { notification } = useSocket();
+
+  useEffect(() => {
+    notification?.onNotification((n) => {
+      setShowDot(true);
+    });
+  }, [notification]);
 
   const handleOpenMenu = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setShowDot(false);
