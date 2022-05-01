f5a165c7642459f8dd0cae4410bf4e07ee56ebf8
mobin <mnlarijanian@gmail.com>
2022-05-02 01:24:57 +0430

    logout button for phone responsive added

--- src/app/TopAppBar/index.tsx
+++ src/app/TopAppBar/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Button, IconButton, Box } from "@material-ui/core";
-import { ArrowDropDownRounded, HelpOutline, TvRounded, ChatRounded } from "@material-ui/icons";
+import { ArrowDropDownRounded, HelpOutline, TvRounded, ChatRounded, ExitToAppRounded } from "@material-ui/icons";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useDispatch } from "react-redux";
+
+import { logout } from "features/Session/sessionsSlice";
 
 import UPCToggle from "../../components/UPC";
 import NotificationButton from "./Notification/Button";
@@ -13,6 +16,7 @@ export const TopAppBar = ({
   onOpenChatClicked: () => void;
   isChatOpen: boolean;
 }) => {
+  const dispatch = useDispatch();
   const phone = useMediaQuery("(max-width:900px)");
 
   return (
@@ -20,6 +24,9 @@ export const TopAppBar = ({
       <div style={{ flexGrow: 1 }}></div>
       {phone && <div style={{ flexGrow: 1 }}></div>}
 
+      <IconButton onClick={() => dispatch(logout())} size="small" title="Help" color="inherit">
+        <ExitToAppRounded style={{ marginRight: 3 }} />
+      </IconButton>
       <UPCToggle />
       {!phone && (
         <IconButton size="small" title="Help" color="inherit">
