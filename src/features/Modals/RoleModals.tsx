2fb150f0ab1a88ec18f6dd0331ce1f35d66b789a
amin23877 <amin23877@gmail.com>
2022-11-05 17:27:27 +0330

    role patching apis

--- src/features/Modals/RoleModals.tsx
+++ src/features/Modals/RoleModals.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Dialog from "app/Dialog";
 
 import { GeneralForm } from "app/Forms";
 import { addRole } from "api/role";
+import useSWR from "swr";
 
 export const AddRoleModal = ({
   open,
@@ -15,16 +16,30 @@ export const AddRoleModal = ({
   setRefresh: any;
   initialVals?: any;
 }) => {
+  const { data: apis } = useSWR(initialVals ? `/role/${initialVals.id}/api` : null);
+
+  const [apiArray, setApiArray] = useState();
+
+  useEffect(() => {
+    if (apis?.result[0]) {
+      let temp = apis.result.map((api: any) => api.apis._id);
+      setApiArray(temp);
+    }
+  }, [apis]);
+
   return (
-    <Dialog open={open} onClose={onClose} title="Add new role" fullScreen>
+    <Dialog open={open} onClose={onClose} title={initialVals ? "Edit Role" : "Add new role"} fullScreen>
       <div>
-        <GeneralForm
-          addRecord={addRole}
-          type="Role"
-          setRefresh={setRefresh}
-          onClose={onClose}
-          initialVals={initialVals}
-        />
+        {initialVals && apiArray && (
+          <GeneralForm
+            addRecord={addRole}
+            type="Role"
+            setRefresh={setRefresh}
+            onClose={onClose}
+            initialVals={{ ...initialVals, apis: apiArray }}
+          />
+        )}
+        {!initialVals && <GeneralForm addRecord={addRole} type="Role" setRefresh={setRefresh} onClose={onClose} />}
       </div>
     </Dialog>
   );
