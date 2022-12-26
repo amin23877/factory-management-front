b6272c198705cd598f86c647b7a54c30762ea9c7
M.ErfanKarami <m.erfankarami1998@gmail.com>
2022-12-26 15:55:03 +0330

    bom table in engineering device fixed

--- src/features/BOM/ItemBomTable.tsx
+++ src/features/BOM/ItemBomTable.tsx
@@ -22,15 +22,7 @@ import BomRecordModal from "./BomRecordModal";
 import { useLock } from "common/Lock";
 import Confirm from "common/Confirm";
 
-export default function ItemBomTable({
-  boms,
-  item,
-  mutateBoms,
-}: {
-  boms?: IBom[];
-  item: IItem;
-  mutateBoms: () => void;
-}) {
+export default function ItemBomTable({ boms, item, mutateBoms }: { boms?: IBom[]; item: any; mutateBoms: () => void }) {
   const [bomModal, setBomModal] = useState(false);
   const [bomRecordModal, setBomRecordModal] = useState(false);
   const [selectedBom, setSelectedBom] = useState<IBom>();
@@ -39,6 +31,8 @@ export default function ItemBomTable({
   const history = useHistory();
   const phone = useMediaQuery("(max-width:900px)");
   const { lock } = useLock();
+
+  const selected = item?.result?.find(() => true);
 
   const handleDelete = useCallback((data: IBom) => {
     Confirm({
@@ -145,7 +139,7 @@ export default function ItemBomTable({
           BOM
         </Button>
       </Box>
-      <DataGrid columns={columns} url={`/bom?ItemId=${item.id}`} onRowSelected={() => {}} refresh={refresh} />
+      <DataGrid columns={columns} url={`/bom?ItemId=${selected.id}`} onRowSelected={() => {}} refresh={refresh} />
     </>
   );
 }
